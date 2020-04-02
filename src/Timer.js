4892f1b6d1765e6add1019ec72c8204b3410c134
Bry Cox <carl.cc@gmail.com>
2020-04-02 09:17:38 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -6,7 +6,7 @@ class Timer extends Component {
     time: 0,
     color: '#'+Math.floor(Math.random()*16777215).toString(16)
   }
-  
+
   componentDidMount() {
     this.interval = setInterval(this.clockTick, 1000)
   }
